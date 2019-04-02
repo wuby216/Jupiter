dc23f59191dec96c06ad6cbd9347beea3ba4957e
Bingyang Wu <bingyang216@gmail.com>
2019-04-02 00:33:17 -0700

    First commit

---
+++ src/rpc/Login.java
@@ -0,0 +1,98 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	// check if session already exists
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DBConnection connection = DBConnectionFactory.getConnection();
+		try {
+			// set input to false indicates if the session doesn't exist, just return null, no need to create a new session
+			// otherwise request.getSession() creates a new session if not exist
+			HttpSession session = request.getSession(false); 
+			
+			JSONObject obj = new JSONObject();
+			if (session != null) { 
+				String userId = session.getAttribute("user_id").toString();
+				obj.put("status", "OK").put("user_id", userId).put("name", connection.getFullname(userId));
+			} else {
+				obj.put("status", "Invalid Session");
+				response.setStatus(403);
+			}
+
+			RpcHelper.writeJsonObject(response, obj);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	// try to log in
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DBConnection connection = DBConnectionFactory.getConnection();
+		try {
+			JSONObject input = RpcHelper.readJSONObject(request);
+			String userId = input.getString("user_id");
+			String password = input.getString("password");
+
+			JSONObject obj = new JSONObject();
+			// create session and return back to client
+			if (connection.verifyLogin(userId, password)) { 
+				HttpSession session = request.getSession(); // session is created and automatically binded to response
+				session.setAttribute("user_id", userId);
+				session.setMaxInactiveInterval(600);    // expiration time
+				obj.put("status", "OK").put("user_id", userId).put("name", connection.getFullname(userId));
+			} else {
+				obj.put("status", "User Doesn't Exist");
+				response.setStatus(401);
+			}
+
+			RpcHelper.writeJsonObject(response, obj);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+	}
+}
